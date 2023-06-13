f1275263b42afa5b3925451292baaf8bd0ce3793
cheng <quanuanc@outlook.com>
2023-06-13 17:49:39 +0800

    Update powershell

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,11 +4,8 @@ function dfmui(){gitui -d $env:USERPROFILE\.dfm -w $env:USERPROFILE $args}
 
 fnm env --use-on-cd | Out-String | Invoke-Expression
 
-# For zoxide v0.8.0+
-Invoke-Expression (& {
-  $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-      (zoxide init --hook $hook powershell | Out-String)
-})
+# Zoxide
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 # Remember install newer PSReadLine by: Install-Module -Force PSReadLine
 Set-PSReadLineOption -EditMode Emacs

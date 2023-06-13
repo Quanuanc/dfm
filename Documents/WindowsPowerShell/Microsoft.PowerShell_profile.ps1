0ac93d3bd764de92e8acc8ddea5efad9047c7555
cheng <quanuanc@outlook.com>
2023-06-13 16:33:45 +0800

    Add win branch

---
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+function dfm(){git --git-dir=$env:USERPROFILE\.dfm --work-tree=$env:USERPROFILE $args}
+
+function dfmui(){gitui -d $env:USERPROFILE\.dfm -w $env:USERPROFILE $args}
+
+fnm env --use-on-cd | Out-String | Invoke-Expression
+
+# For zoxide v0.8.0+
+Invoke-Expression (& {
+  $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+      (zoxide init --hook $hook powershell | Out-String)
+})
+
+Set-PSReadLineOption -EditMode Emacs

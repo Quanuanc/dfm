011993427559f6d4e3a643f02b0b743fcba0154b
Cheng <quanuanc@outlook.com>
2023-08-26 15:23:08 +0800

    Update

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,11 +2,12 @@ function dfm(){git --git-dir=$env:USERPROFILE\.dfm --work-tree=$env:USERPROFILE 
 
 function dfmui(){gitui -d $env:USERPROFILE\.dfm -w $env:USERPROFILE $args}
 
-fnm env --use-on-cd | Out-String | Invoke-Expression
-
-# Zoxide
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
 # Remember install newer PSReadLine by: Install-Module -Force PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord "Ctrl+e" -Function ForwardWord
+
+$fnmExists = Get-Command -Name fnm -ErrorAction SilentlyContinue
+if($fnmExists){ fnm env --use-on-cd | Out-String | Invoke-Expression }
+
+$zoxideExists = Get-Command -Name zoxide -ErrorAction SilentlyContinue
+if($zoxideExists){ Invoke-Expression (& { (zoxide init powershell | Out-String) }) }
